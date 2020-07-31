f9711f5468c208b50f811b8dc9afb98afe480a9f
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 01:03:34 -0300

    Aula 05 - Objetos

---
+++ 05-objetos/objetos.js
@@ -0,0 +1,16 @@
+console.log("Objetos");
+
+const PESSOA = {
+    nome: 'Silvano',
+    sobrenome: 'Pimentel',
+    idade: 15,
+    altura: 1.71,
+    serie: '1* A'
+};
+
+console.log(PESSOA);
+console.log("Nome: " + PESSOA.nome);
+console.log("Sobrenome: " + PESSOA.sobrenome);
+console.log("Idade: " + PESSOA.idade);
+console.log("Altura: " + PESSOA.altura);
+console.log("Série: " + PESSOA.serie);
