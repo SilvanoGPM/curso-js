6a2c82d3bab6d8884426df3a9a10d554841b3ccc
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 02:52:34 -0300

    Mini Projeto 01 - Trocar Valores

---
+++ miniprojetos/01-trocar-valores/trocar-valores.js
@@ -0,0 +1,17 @@
+console.log("Trocar Valores");
+
+let a = 'Vermelho';
+let b = 'Azul';
+
+console.log("Valor de a: " + a);
+console.log("Valor de b: " + b);
+
+console.log();
+console.log("Troca");
+console.log();
+
+let aux = a;
+a = b;
+b = aux;
+console.log("Valor de a: " + a);
+console.log("Valor de b: " + b);
