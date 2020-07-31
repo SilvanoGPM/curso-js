69520cf47df245e53307633d5451a3d03fa8282d
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 00:52:10 -0300

    Aula 04 - Tipagem Dinamica

---
+++ 04-tipagem-dinamica/tipagem-dinamica.js
@@ -0,0 +1,13 @@
+console.log("Tipagem Dinâmica");
+
+const number = 0;
+const string = "Olá";
+const boolean = true;
+const indefinido = undefined;
+const object = null;
+
+console.log("Tipo da variável number: " + typeof number);
+console.log("Tipo da variável string: " + typeof string);
+console.log("Tipo da variável boolean: " + typeof boolean);
+console.log("Tipo da variável indefinido: " + typeof indefinido);
+console.log("Tipo da variável object: " + typeof object);
