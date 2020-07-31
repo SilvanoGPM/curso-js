a728f340014dd2d23845e4f52c4bcaaa30481469
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 00:14:33 -0300

    Aula 01 - Variaveis

---
+++ 01-variaveis/variaveis.js
@@ -0,0 +1,9 @@
+console.log("Variáveis");
+
+let nome = "Silvano Gabriel";
+let idade = 15;
+let numeroTelefone = "00 12345678";
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("N de telefone: " + numeroTelefone);
