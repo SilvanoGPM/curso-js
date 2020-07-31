4dbb8f474aba758b564703c09baab3c406fbbced
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 00:27:46 -0300

    Aula 02 - Constantes

---
+++ 02-constantes/constantes.js
@@ -0,0 +1,13 @@
+console.log("Constantes");
+
+const PI = 3.14;
+const HORAS_DIA = 24;
+const DIAS_SEMANA = 7;
+const MINUTOS_EM_UMA_HORA = 60;
+const DESCOBRIDOR_DO_BRASIL = "Pedro Álvares Cabral";
+
+console.log("Valor de PI: " + PI);
+console.log("Horas em um dia: " + HORAS_DIA);
+console.log("Dias da semana: " + DIAS_SEMANA);
+console.log("Minutos em uma hora: " + MINUTOS_EM_UMA_HORA);
+console.log("Descobridor do Brasil: " + DESCOBRIDOR_DO_BRASIL)
