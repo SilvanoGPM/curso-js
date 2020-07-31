08d4318c045a5b92c02b579098292ee2e6b5bc66
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 01:45:13 -0300

    Aula 08 - Tipos de Funcao

---
+++ 08-tipos-de-funcao/tipos-de-funcao.js
@@ -0,0 +1,15 @@
+console.log("Tipos de Função");
+
+// Função que somete executa um trecho de código, sem retorno.
+function fazerSaudacao(nome) {
+    console.log("Olá, " + nome + "!");
+}
+
+// Função que executa um trecho de código e retorna um valor.
+function somar(n, m) {
+    return n + m;
+}
+
+fazerSaudacao("Silvano");
+console.log(somar(10, 10));
+
