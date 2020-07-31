268cd06d10d0a09ffd4647535fa569bdb74b41b9
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 00:41:30 -0300

    Aula 03 - Tipos Primitivos

---
+++ 03-tipos-primitivos/tipos-primitivos.js
@@ -0,0 +1,19 @@
+console.log("Tipos Primitivos");
+
+const number0 = 63;
+const number1 = 13.13;
+const string0 = "Hello, World";
+const string1 = "Curso JavaScript";
+const boolean0 = true;
+const boolean1 = false;
+const indefinido = undefined;
+const nulo = null;
+
+console.log("Number literal: " + number0);
+console.log("Number literal: " + number1);
+console.log("String literal: " + string0);
+console.log("String literal: " + string1);
+console.log("Booleano: " + boolean0);
+console.log("Booleano: " + boolean1);
+console.log("Indefinido: " + indefinido);
+console.log("Nulo: " + nulo)
