3768f9e6368fa83f113c685085eebb1ebfd35221
SkyG0D <silvanosilvino@hotmail.com>
2020-07-31 01:20:57 -0300

    Aula 06 - Arrays

---
+++ 06-arrays/arrays.js
@@ -0,0 +1,8 @@
+console.log("Arrays");
+
+const ARRAY = ["Gustavo", 17, "Salvador"];
+
+console.log(ARRAY);
+console.log("Nome: " + ARRAY[0]);
+console.log("Idade: " + ARRAY[1]);
+console.log("Cidade: " + ARRAY[2]);
