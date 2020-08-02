42790fca9a3d18884613d118277d60be3d44350b
SkyG0D <silvanosilvino@hotmail.com>
2020-08-01 21:28:43 -0300

    Aula 18 - Basico do DOM

---
+++ 18-dom/dom.js
@@ -0,0 +1,14 @@
+console.log("DOM");
+
+const TITULO = document.getElementById('titulo');
+
+const SUBTITULO1 = document.getElementById('subtitulo1');
+
+const SUBTITULOS = document.getElementsByClassName('subtitulo');
+
+const PARAGRAFOS = document.getElementsByTagName('p');
+
+const SPANS = document.getElementsByTagName('span');
+
+const DIVS = document.getElementsByTagName('div');
+
